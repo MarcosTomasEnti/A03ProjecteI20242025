91fd2769f5b86db495e0106b8573d50502a4c2f4
MarcosTomasEnti <Marcos.tomas@enti.cat>
2025-03-10 22:25:07 +0100

    Minor changes + fixes
    -fixed layers not saved correctly
    -the boncy ball test attack now has a variable for the times it can bounce
    -all shots now dissapear after some time existing to prevent laggyness
    -Main camera's size changed from 12 to 20.

--- Assets/Scripts/Bounch ball.cs
+++ Assets/Scripts/Bounch ball.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class Bounchball : MonoBehaviour
 {
-    private int Rebotes = 0;
+    public int Rebotes = 2;
     private bool hitEnemy = false;
     // Start is called before the first frame update
     void Start()
     {
-
+        Destroy(gameObject, 10);
     }
 
     // Update is called once per frame
@@ -19,11 +19,11 @@ public class Bounchball : MonoBehaviour
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if ((collision.gameObject.CompareTag("Suelo") && Rebotes == 0))
+        if ((collision.gameObject.CompareTag("Suelo") && Rebotes > 0))
         {
-            Rebotes++;
+            Rebotes--;
         }
-        else if(collision.gameObject.CompareTag("Enemigo")|| (collision.gameObject.CompareTag("Suelo") && Rebotes == 1))
+        else if(collision.gameObject.CompareTag("Enemigo") || (collision.gameObject.CompareTag("Suelo") && Rebotes == 0))
         {
             Destroy(gameObject);
         }
--- Assets/Scripts/FireBall.cs
+++ Assets/Scripts/FireBall.cs
@@ -8,7 +8,7 @@ public class FireBall : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Destroy(gameObject, 6f);
     }
 
     // Update is called once per frame
