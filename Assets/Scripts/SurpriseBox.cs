089070f510f755984df793c50df910e08e4691f0
EdgardHernandezENTI <edgard.hernandez@enti.cat>
2025-04-01 12:04:37 +0200

    Cajas Mejoradas con dropeo de Vida

--- Assets/Scripts/SurpriseBox.cs
+++ Assets/Scripts/SurpriseBox.cs
@@ -6,18 +6,25 @@ using UnityEngine;
 public class SurpriseBox : MonoBehaviour
 {
     public GameObject Enemy;
+    public GameObject Heart;
     // Start is called before the first frame update
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("Entro");
+   
 
          if (collision.gameObject.CompareTag("Bullet"))
         {
-            int goodLuck = Random.Range(0, 2);
+            int goodLuck = Random.Range(0, 3);
+          
 
             if (goodLuck == 1)
             {
                 GameObject enemy = Instantiate(Enemy.gameObject, transform.position, transform.rotation);
+                Destroy(gameObject.gameObject);
+            }
+            else if (goodLuck == 2)
+            {
+                GameObject heart = Instantiate(Heart.gameObject, transform.position, transform.rotation);
                 Destroy(gameObject.gameObject);
             }
             else
@@ -29,14 +36,20 @@ public class SurpriseBox : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("Entro2");
+       
         if (collision.gameObject.CompareTag("Bullet"))
         {
-            int goodLuck = Random.Range(0, 2);
-            Debug.Log(goodLuck);
+            int goodLuck = Random.Range(0, 3);
+           
+          
             if (goodLuck == 1)
             {
                 GameObject enemy = Instantiate(Enemy.gameObject, transform.position, transform.rotation);
+                Destroy(gameObject.gameObject);
+            }
+            else if (goodLuck == 2)
+            {
+                GameObject heart = Instantiate(Heart.gameObject, transform.position, transform.rotation);
                 Destroy(gameObject.gameObject);
             }
             else
