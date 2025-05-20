810d7afc6c246ed0f25e2efd97949a15953763aa
EricSebastianesENTI <eric.sebastianes@enti.cat>
2025-05-20 06:48:54 +0200

    Pilares funcionales
    Los pilares activan puertas especiales (falta corregir que el sprite de la puerta desaparece)

--- Assets/Scripts/MagicDoor.cs
+++ Assets/Scripts/MagicDoor.cs
@@ -5,53 +5,27 @@ using UnityEngine;
 
 public class MagicDoor : MonoBehaviour
 {
-    public bool lockedMagic = false;
-
-    int collisionStayCount = 0;
-    [HideInInspector]
-    public GameObject magician;
-
     public Sprite openGoldDoor;
     public Sprite closeGoldDoor;
-    GameObject pilares;
 
-    SpriteRenderer sprite;
-    BoxCollider2D boxCollider;
+    private SpriteRenderer sprite;
+    private BoxCollider2D boxCollider;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        /*  if (lockedMagic == true && pilares.GetComponent<Pilares>().activado == true)
-          {
-              openTheDoor();
-              collisionStayCount++;
-              Debug.Log(collisionStayCount);
-          }*/
-
-        if (pilares.GetComponent<Pilares>().activado == true)
-        {
-            //openTheDoor();
-        }
-    }
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        collisionStayCount--;
-        Debug.Log(collisionStayCount);
-        if (collisionStayCount <= 0)
-        {
-            closeTheDoor();
-            collisionStayCount = 0;
-        }
+        sprite = GetComponent<SpriteRenderer>();
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
-    void openTheDoor()
+    public void AbrirPuertaMagica()
     {
         sprite.sprite = openGoldDoor;
         boxCollider.enabled = false;
     }
-    void closeTheDoor()
+
+    public void CerrarPuertaMagica()
     {
-       sprite.sprite = closeGoldDoor;
-       boxCollider.enabled = true;
+        sprite.sprite = closeGoldDoor;
+        boxCollider.enabled = true;
     }
 }
--- Assets/Scripts/Pilares.cs
+++ Assets/Scripts/Pilares.cs
@@ -4,18 +4,24 @@ using UnityEngine;
 
 public class Pilares : MonoBehaviour
 {
-    public bool activado = false;
-    //public bool lockedMagic = false;
-    float tiempoRestante;
-    float tiempo = 10f; //El tiempo máximo que estara activado
-    GameObject puerta;
 
-    // Update is called once per frame
-    void Update()
+    public bool activado = false;
+    public float tiempoActivo = 10f; // tiempo máximo activado
+    private float tiempoRestante = 0f;
+
+    public MagicDoor puertaVinculada; // Asignar en el inspector
+
+    private void Update()
     {
-        if (activado == true && (Time.deltaTime - tiempoRestante) < tiempo)
-        {           
-          activado = false;            
+        if (activado)
+        {
+            tiempoRestante -= Time.deltaTime;
+
+            if (tiempoRestante <= 0f)
+            {
+                activado = false;
+                puertaVinculada.CerrarPuertaMagica();
+            }
         }
     }
 
@@ -24,8 +30,10 @@ public class Pilares : MonoBehaviour
         if (collision.gameObject.CompareTag("Bullet"))
         {
             activado = true;
-            //puerta.GetComponent<MagicDoor>().lockedMagic == true;
-            tiempoRestante += Time.deltaTime;
+            tiempoRestante = tiempoActivo;
+
+            puertaVinculada.AbrirPuertaMagica();
+            Destroy(collision.gameObject);
         }
-    }
+    }    
 }
