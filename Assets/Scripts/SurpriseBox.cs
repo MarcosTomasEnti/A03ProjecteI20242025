f07a4de68a0264250815f1f5411e3cc05ced3f16
MarcosTomasEnti <Marcos.tomas@enti.cat>
2025-06-02 22:46:30 +0200

    cambio balance cajas

--- Assets/Scripts/SurpriseBox.cs
+++ Assets/Scripts/SurpriseBox.cs
@@ -67,37 +67,37 @@ public class SurpriseBox : MonoBehaviour
 
         if (collision.gameObject.CompareTag("Bullet"))
         {
-            int goodLuck = Random.Range(0, 7);
+            float goodLuck = Random.Range(0, 120);//hearts: 10% enemies: 30% coins: 10% mana potion: 10%
             Debug.Log(goodLuck);
 
-            if (goodLuck == 1)
+            if (goodLuck >= 0 && goodLuck < 10)
             {
                 GameObject enemy = Instantiate(meleeEnemy.gameObject, transform.position, transform.rotation);
                 Destroy(gameObject.gameObject);
             }
-            else if (goodLuck == 2)
+            else if (goodLuck >= 10 && goodLuck < 20)
             {
                 GameObject heart = Instantiate(Heart.gameObject, transform.position, transform.rotation);
                 Destroy(gameObject.gameObject);
             }
-            else if (goodLuck == 3)
+            else if (goodLuck >= 20 && goodLuck < 30)
             {
                 GameObject enemy = Instantiate(rangedEnemy.gameObject, transform.position, transform.rotation);
                 Destroy(gameObject.gameObject);
             }
-            else if ((goodLuck == 4))
+            else if (goodLuck >= 30 && goodLuck < 40)
             {
                 GameObject coin = Instantiate(Coin.gameObject, transform.position, transform.rotation);
                 Destroy(gameObject.gameObject);
             }
-            else if ((goodLuck == 5))
+            else if (goodLuck >= 40 && goodLuck < 50)
             {
                 GameObject potionMana = Instantiate(PotionMana.gameObject, transform.position, transform.rotation);
                 Destroy(gameObject.gameObject);
             }
-            else if ((goodLuck == 6))
+            else if (goodLuck >= 50 && goodLuck < 60)
             {
-                int flyChance = Random.Range(1, 6);
+                int flyChance = Random.Range(3, 6);
                 for (int i = 0; i < flyChance; i++)
                 {
                     GameObject fly = Instantiate(Fly.gameObject, transform.position, transform.rotation);
